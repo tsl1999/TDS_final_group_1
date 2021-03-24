c9e3ef21726d85c2d97774e250659170dc3fca88
Tianshu Liu <tl1020@login-9.ib0.cx2.hpc.ic.ac.uk>
2021-03-24 00:13:17 +0000

    step 3

--- codes/3_03safety_Check_prs.R
+++ codes/3_03safety_Check_prs.R
@@ -2,12 +2,25 @@
 # 1 nutrition score and lung cancer
 
 library(data.table)
-setwd('/rds/general/project/hda_students_data/live/Group1/tds_final_group_1/result_data/step2')
-data1<-readRDS("/rds/general/project/hda_students_data/live/Group1/tds_final_group_1/result_data/step1/combined_nutrition_code.rds")
+data1<-readRDS("/rds/general/project/hda_students_data/live/Group1/TDS_final_group_1/result_data/step1/combined_nutrition_code.rds")
+prs<-readRDS("/rds/general/project/hda_students_data/live/Group1/TDS_final_group_1/result_data/step3/prs.rds")
+GWAS_PCs<-readRDS('/rds/general/project/hda_students_data/live/Group1/TDS_final_group_1/data/GWAS_PCs.rds')
+mr<-no_na_nu[,c("eid","BMI","NutritionScore","sex","Smoking","age","LungCancer","alcohol_intake_frequency","ColonCancer","StomachCancer","BreastCancer","HDL_Cholesterol")]
+rownames(mr)<-mr$eid
+mr_prs<-mr[rownames(mr)%in%data1$eid,]
+rownames(GWAS_PCs)<-GWAS_PCs$eid
+PC_n<-GWAS_PCs[rownames(GWAS_PCs)%in%data1$eid,1:11]
+mr_prs<-merge(mr_prs,PC_n,by="eid")
+mr_prs<-merge(mr_prs,prs,by="eid")
+rownames(mr_prs)<-mr_prs$eid
+no_na_nu<-data1
+mr_prs$LungCancer<-ifelse(mr_prs$LungCancer=="no",0,1)
+nutrition_prs=mr_prs
 
-no_na_nu_age<-data
-data1$LungCancer<-ifelse(data1$LungCancer=="no",0,1)
-e<-glm(LungCancer~NutritionScore+age+sex+Smoking,data=data1,family="binomial")
+e<-glm(LungCancer~NutritionScore+age+sex+Smoking,data=mr_prs,family="binomial")
+summary(e)#stats significant
+
+e<-glm(BreastCancer~NutritionScore+age+sex+Smoking,data=data1,family="binomial")
 summary(e)#stats significant
 
 e<-lm(no_na_nu_age$NutritionScore~no_na_nu_age$LungCancer+no_na_nu_age$age+no_na_nu_age$sex+no_na_nu_age$Smoking)
@@ -19,7 +32,7 @@ t.test(data1$NutritionScore,data1$LungCancer)#different in mean
 #2 nutrition score and prs
 
 cor(nutrition_prs$NutritionScore,nutrition_prs$prs,use="complete.obs")#-0.07, very low
-b<-lm(nutrition_prs$NutritionScore~.,data=nutrition_prs[,7:17])
+b<-lm(mr_prs$NutritionScore~.,data=mr_prs[,c(13:23)])
 summary(b)#with pcs, r squred 2% ish, significant prs, f stats significant
 
 e<-lm(nutrition_prs$NutritionScore~ nutrition_prs$prs)
@@ -36,7 +49,7 @@ summary(e)#adjust for smoking, 9%, not 1%
 #prs and BMI
 
 cor(nutrition_prs$prs,nutrition_prs$BMI,use="complete.obs")#-0.003
-e<-lm(BMI~prs+.,data=nutrition_prs[,c(5,7:17)])
+e<-lm(BMI~prs+.,data=mr_prs[,c(2,13:23)])
 summary(e)#stats significant but low r squred, with pcs, 0.6%
 
 #nutrition score and HDL cholesterol
@@ -48,20 +61,20 @@ summary(e)#with smoking 9%, without 7%, stats significant
 #prs and HDL cholesterol
 
 cor(nutrition_prs$HDL_Cholesterol,nutrition_prs$prs,use="complete.obs")#0.016
-e<-lm(HDL_Cholesterol~prs+.,data=nutrition_prs[,c(6,17)])
+e<-lm(HDL_Cholesterol~prs+.,data=mr_prs[,c(12:23)])
 summary(e)#low r squred but stats significant prs
 
 #prs and lung cancer
-e<-glm(LungCancer~ prs+.,data=mr_prs[,c(2,6:16)],family="binomial")
+e<-glm(LungCancer~ prs+.,data=mr_prs[,c(7,13:23)],family="binomial")
 summary(e)#not significant
 boxplot(mr_prs$prs~mr_prs$LungCancer)
 t.test(mr_prs$prs,mr_prs$LungCancer)#significant mean difference
 
 #prs and other cancer
 #Breast
-e<-glm(BreastCancer~ prs+.,data=mr_prs[,c(5,6:16)],family="binomial")
+e<-glm(BreastCancer~ prs+.,data=mr_prs[,c(11,13:23)],family="binomial")
 summary(e)#significant prs, with or without pcs
-e<-lm(prs~ BreastCancer+.,data=mr_prs[,c(5,6:16)])
+e<-lm(prs~ BreastCancer+.,data=mr_prs[,c(11,13:23)])
 summary(e)
 
 t.test(mr_prs$prs,mr_prs$BreastCancer)#significant mean difference
@@ -69,9 +82,8 @@ t.test(mr_prs$prs,mr_prs$BreastCancer)#significant mean difference
 boxplot(mr_prs$prs~mr_prs$BreastCancer)
 
 #stomach
-e<-glm(StomachCancer~ prs+.,data=mr_prs[,c(4,6:16)],family="binomial")
+e<-glm(StomachCancer~ prs+.,data=mr_prs[,c(10,13:23)],family="binomial")
 summary(e)#not significant
-e<-lm(prs~ StomachCancer+.,data=mr_prs[,c(4,6:16)])
-summary(e)
+
 boxplot(mr_prs$prs~mr_prs$StomachCancer)
 t.test(mr_prs$prs,mr_prs$StomachCancer)
